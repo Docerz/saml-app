9ede6c1c3fd76f16d24c1bfa285dcda9b6989940
mohammed-js-khan <mohammed.jskhan@gmail.com>
2018-02-27 17:02:49 -0500

    Added some session sec, added helmet for other sec, cleaned up some html, added rolling session @30min

--- server.js
+++ server.js
@@ -6,20 +6,35 @@ var express  = require('express');
 var app      = express();
 var port     = process.env.PORT || 8080;
 
+var helmet       = require('helmet')
 var morgan       = require('morgan');
-var cookieParser = require('cookie-parser');
 var bodyParser   = require('body-parser');
 var session      = require('express-session');
 
 // set up our express application
+app.use(helmet()); // do security stuff
 app.use(morgan('dev')); // log every request to the console
-app.use(cookieParser()); // read cookies (needed for auth)
-app.use(bodyParser()); // get information from html forms
 
+// get information from html forms
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+// session security stuff
 app.use(session({
     secret: 'docers',
-    name: 'sessionId'
-})); // session secret
+    name: 'sessionId',
+    resave: true,
+    saveUninitialized: true,
+    cookie: {
+        //secure: true,
+        httpOnly: true,
+        //domain: 'example.com',
+        //path: 'foo/bar',
+        maxAge: 30 * 60 * 1000 // 30 minutes
+    },
+    rolling: true
+}));
 
 // content
 app.use(express.static(__dirname + '/public'));
