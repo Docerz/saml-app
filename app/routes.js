bff453f04d45af7a81845aa2c551cc382024f678
mohammed-js-khan <mohammed.jskhan@gmail.com>
2018-02-27 14:19:02 -0500

    Added login and logout

--- app/routes.js
+++ app/routes.js
@@ -3,36 +3,59 @@ var path = require("path");
 // app/routes.js
 module.exports = function(app, passport) {
 
-    // =====================================
-    // HOME PAGE (with login links) ========
-    // =====================================
+
+    // LANDING PAGE (with the login link)
     app.get('/', function(req, res) {
-        res.sendFile(path.join(__dirname + '/../views/index.html')); // load the index.html file
+        if ('user' in req.session) {
+            res.sendFile(path.join(__dirname + '/../views/home.html'));
+        } else {
+            res.sendFile(path.join(__dirname + '/../views/index.html'));
+        }
     });
 
-    // =====================================
-    // LOGIN ===============================
-    // =====================================
-    // show the login form
-    app.get('/login', function(req, res) {
 
-        // render the page and pass in any flash data if it exists
+    // LOGIN PAGE (with email, password, login, cancel)
+    app.get('/login', function(req, res) {
         res.sendFile(path.join(__dirname + '/../views/login.html')); 
     });
 
-    // =====================================
-    // LOGOUT ==============================
-    // =====================================
-    app.get('/logout', function(req, res) {
-        req.logout();
-        res.redirect('/');
+
+    // LOGIN FUNCTION (authenticates, sets session)
+    app.post('/login', function(req, res){
+        if ('user' in req.session) {
+            req.session.destroy();
+            return res.status(400).send('invalid request');
+        }
+    
+        if (!req.body.username || !req.body.password) {
+            return res.status(400).send('missing requirement');
+        }
+    
+        var username = req.body.username.toLowerCase();
+        var password = req.body.password;
+
+        if (username === 'momos' && password === 'fro') {
+            req.session.user = username;
+            res.redirect('/home');
+        } else {
+            return res.status(403).send('incorrect credentials');
+        }
     });
 
-    // =====================================
-    // HOME ==============================
-    // =====================================
+
+    // LOGOUT (truncate session, and redirect to landing page)
+    app.get('/logout', function(req, res) {
+        if (req.session.user) {
+            req.session.destroy();
+        }
+    
+        return res.redirect('/');
+    });
+
+
+    // HOME PAGE (once authenticated only)
     app.get('/home', function(req, res) {
-        isLoggedIn(req, res, '/home')
+        isLoggedIn(req, res, 'home.html')
     });
 };
 
@@ -40,8 +63,8 @@ module.exports = function(app, passport) {
 function isLoggedIn(req, res, next) {
 
     // if user is authenticated in the session, carry on 
-    if (req.isAuthenticated())
-        return next();
+    if (req.session.user)
+        return res.sendFile(path.join(__dirname + '/../views/' + next));
 
     // if they aren't redirect them to the home page
     res.redirect('/');
--- server.js
+++ server.js
@@ -5,31 +5,27 @@
 var express  = require('express');
 var app      = express();
 var port     = process.env.PORT || 8080;
-var passport = require('passport');
 
 var morgan       = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser   = require('body-parser');
 var session      = require('express-session');
 
-
-// require('./config/passport')(passport); // pass passport for configuration
-
 // set up our express application
 app.use(morgan('dev')); // log every request to the console
 app.use(cookieParser()); // read cookies (needed for auth)
 app.use(bodyParser()); // get information from html forms
 
-// required for passport
-app.use(session({ secret: 'docers' })); // session secret
-app.use(passport.initialize());
-app.use(passport.session()); // persistent login sessions
+app.use(session({
+    secret: 'docers',
+    name: 'sessionId'
+})); // session secret
 
 // content
 app.use(express.static(__dirname + '/public'));
 
 // routes ======================================================================
-require('./app/routes.js')(app, passport); // load our routes and pass in our app and fully configured passport
+require('./app/routes.js')(app); // load our routes and pass in our app and fully configured passport
 
 
 // launch ======================================================================
