096e10abba578568f961b376bba64ef51c6db47a
Mohamed-A-Mohamed <mohamed.a.mohamed1117@gmail.com>
2018-02-26 22:44:36 -0500

    Setting up server

---
+++ server.js
@@ -0,0 +1,32 @@
+// server.js
+
+// set up ======================================================================
+// get all the tools we need
+var express  = require('express');
+var app      = express();
+var port     = process.env.PORT || 8080;
+var passport = require('passport');
+
+var morgan       = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser   = require('body-parser');
+
+
+// require('./config/passport')(passport); // pass passport for configuration
+
+// set up our express application
+app.use(morgan('dev')); // log every request to the console
+app.use(cookieParser()); // read cookies (needed for auth)
+app.use(bodyParser()); // get information from html forms
+
+// required for passport
+app.use(session({ secret: 'docers' })); // session secret
+app.use(passport.initialize());
+app.use(passport.session()); // persistent login sessions
+
+// routes ======================================================================
+require('./app/routes.js')(app, passport); // load our routes and pass in our app and fully configured passport
+
+// launch ======================================================================
+app.listen(port);
+console.log('The magic happens on port ' + port);
